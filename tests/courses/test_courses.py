acce01fac2f94dfbf581155ef232880da29155fe
Andrey Bozhko <556070@gmail.com>
2025-11-09 16:33:06 +0300

    7.7 Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -51,3 +51,50 @@ class TestCourses:
             index=0, title=title, max_score=max_score, min_score=min_score, estimated_time=estimated_time
         )
 
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+
+        title = "Playwright"
+        description = "Playwright"
+        estimated_time = "2 weeks"
+        max_score = "100"
+        min_score = "10"
+
+        create_course_page.create_course_form.fill(
+            title=title,
+            estimated_time=estimated_time,
+            description=description,
+            max_score=max_score,
+            min_score=min_score
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0, title=title, max_score=max_score, min_score=min_score, estimated_time=estimated_time
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        new_title = "Playwright New"
+        new_description = "Playwright New"
+        new_estimated_time = "3 weeks"
+        new_max_score = "110"
+        new_min_score = "20"
+
+        create_course_page.create_course_form.fill(
+            title=new_title,
+            estimated_time=new_estimated_time,
+            description=new_description,
+            max_score=new_max_score,
+            min_score=new_min_score
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title=new_title,
+            max_score=new_max_score,
+            min_score=new_min_score,
+            estimated_time=new_estimated_time
+        )
+
