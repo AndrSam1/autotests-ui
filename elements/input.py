86a0590e84c830f6a2684f7fbeb05dedde7688f5
Andrey Bozhko <556070@gmail.com>
2025-12-14 18:58:51 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/input.py
+++ elements/input.py
@@ -1,7 +1,10 @@
 import allure
 from playwright.sync_api import Locator, expect
 
+from ui_coverage_tool import ActionType
+
 from elements.base_element import BaseElement
+from tools.logger import get_logger
 
 from tools.logger import get_logger
 
@@ -17,6 +20,13 @@ class Input(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        # Переопределяем метод формирования XPath-селектора:
+        #  - сначала получаем общий селектор блока
+        #  - затем уточняем путь до самого <input>, добавляя '//input'
+        # Это нужно, чтобы трекер точно знал, с каким элементом шло взаимодействие.
+        return f'{super().get_raw_locator(**kwargs)}//input'
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Filling {self.type_of} "{self.name}" to value "{value}"'
 
@@ -25,6 +35,8 @@ class Input(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
 
@@ -32,3 +44,5 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
